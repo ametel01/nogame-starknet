35dc783bc65805cde7f818cea67524369ab50556
ametel01 <alex-metelli@gmx.com>
2023-12-14 19:33:40 +0800

    updated contract class hash on deploy

--- scripts/starkli-testnet-deploy.sh
+++ scripts/starkli-testnet-deploy.sh
@@ -2,7 +2,7 @@
 eth_addr=0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7
 receiver=0x04b053f79a856f8ba34397b4dd540c3a9600aef13dddf5d976bc8547d1438e54
 deployer=0x482e3154eeec94b161d40ac49b29d1da71d7184e004f3f752b7aea57991cf2
-speed=100
+speed=200
 price=0
 
 export STARKNET_RPC="https://starknet-goerli.infura.io/v3/25371764a3e44191b39d3b3b98a8c55d"
@@ -10,12 +10,12 @@ export STARKNET_KEYSTORE=".keystore.json"
 export STARKNET_ACCOUNT="testnet.json"
 
 echo 'deploying NoGame...'
-stdout=$(starkli deploy --watch 0x07d59df63ad1ed4ec8adb122e6c15ac0288f5319e78bcbf1587d0cc5c324d975) 
+stdout=$(starkli deploy --watch 0x0148172b369b7eb959f1292f3fe2132a82fbe2c043f429299a31b12bb8caa21a) 
 nogame=$(echo "$stdout" | grep -o '0x[0-9a-fA-F]\+')
 echo 'NOGAME::'${nogame} > deployed_contracts.txt
 
 echo 'deploying NFT...'
-stdout=$(starkli deploy --watch 0x04bd8ee9efaf20e299720f26a6afc0cdead980a37d1f7fe60c98610874160a0f 0x4e6f47616d65207465737420506c616e6574 0x4e47744e4654 $nogame $receiver)
+stdout=$(starkli deploy --watch 0x001feb052b2ea7b1f744692306346ebc70ac966fd3a55774f9e576ca435bd959 0x4e6f47616d65207465737420506c616e6574 0x4e47744e4654 $nogame $receiver)
 nft=$(echo "$stdout" | grep -o '0x[0-9a-fA-F]\+')
 echo 'NFT::'${nft} >> deployed_contracts.txt
 
